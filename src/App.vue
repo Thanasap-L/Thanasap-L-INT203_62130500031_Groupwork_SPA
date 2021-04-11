c8f024983b1b4c9f135e8715abb64edff891067e
Thanasap-L <thanasap.aloy@mail.kmutt.ac.th>
2021-04-12 00:33:47 +0700

    add db.json

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="flex flex-wrap w-full bg-black">
+    <div class="mx-auto font-bold text-5xl py-1 text-white">
+      <router-link to="/">Music Time</router-link>
+    </div>
+    <div class="navButton">
+      <router-link to="/"> Add Items </router-link>
+      <router-link></router-link>
+      <router-link></router-link>
+    </div>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
